170ee80f0a02b8e4542989503979cd570e9dcbfc
Varvara Babkova <varvara.munday@gmail.com>
2019-06-10 14:13:04 +0000

    Automatically backed up by Learn

--- tweet_shortener.rb
+++ tweet_shortener.rb
@@ -26,7 +26,7 @@ def word_substituter (tweet)
     if dictionary(word) != nil
       word = dictionary(word)
     end
-    str += word + " "
+    #str += word + " "
   end
 
   return str#tweet.join(" ")
