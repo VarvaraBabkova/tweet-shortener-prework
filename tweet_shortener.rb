2c286d2afe0709881646219fd8faa89d53186159
Varvara Babkova <varvara.munday@gmail.com>
2019-06-10 13:52:29 +0000

    Automatically backed up by Learn

--- tweet_shortener.rb
+++ tweet_shortener.rb
@@ -1,18 +1,18 @@
 
 def dictionary (word)
   dict = {
-    "hello" : 'hi',
-    "to" : '2',
-    "two" : '2',
-    "too" : '2' ,
-    "for" : '4',
-    "four" : '4',
-    'be' : 'b',
-    'you' : 'u',
-    "at" : "@" ,
-    "and" : "&"
+    "hello" => 'hi',
+    "to" => '2',
+    "two" => '2',
+    "too" => '2' ,
+    "for" => '4',
+    "four" => '4',
+    'be' => 'b',
+    'you' => 'u',
+    "at" => "@" ,
+    "and" => "&"
   }
   return dict[word]
-end 
+end
 def word_substituter (messsage)
 end
