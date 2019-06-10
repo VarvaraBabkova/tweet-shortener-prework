8dba13bcc376503886362487d3078ed5b8f29297
Varvara Babkova <varvara.munday@gmail.com>
2019-06-10 14:29:54 +0000

    Automatically backed up by Learn

--- tweet_shortener.rb
+++ tweet_shortener.rb
@@ -48,4 +48,12 @@ def selective_tweet_shortener (tweet)
 #  end
 end
 
+def shortened_tweet_truncator(tweet)
+  new_tweet = word_substituter(tweet)
+  if new_tweet.length > 140
+    new_tweet = new_tweet[0..140]
+  end
+  return new_tweet
+end
+
 word_substituter("Hey guys, can anyone teach me how to be cool? I really want to be the best at everything, you know what I mean? Tweeting is super fun you guys!!!!")
