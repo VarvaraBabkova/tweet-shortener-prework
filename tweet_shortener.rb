efa6923fd0b8f2370476609121b13d6df17eccf8
Varvara Babkova <varvara.munday@gmail.com>
2019-06-10 14:12:24 +0000

    Automatically backed up by Learn

--- tweet_shortener.rb
+++ tweet_shortener.rb
@@ -31,3 +31,5 @@ def word_substituter (tweet)
 
   return str#tweet.join(" ")
 end
+
+word_substituter("Hey guys, can anyone teach me how to be cool? I really want to be the best at everything, you know what I mean? Tweeting is super fun you guys!!!!")
